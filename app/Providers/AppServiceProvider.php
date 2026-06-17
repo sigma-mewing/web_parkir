68d278f601cf3040737ec56f010fca64e68ec888
sigma-mewing <sigma.mewing1280@gmail.com>
2026-06-17 16:36:07 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Request::setTrustedProxies(
+            ['172.20.2.18', '127.0.0.1'],  // IP HAProxy / VM1
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO
+        );
+
+        // Force semua URL Laravel pakai https://
+        if (env('FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
     }
 }
